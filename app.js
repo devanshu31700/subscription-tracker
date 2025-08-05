0ecf04580c562b02e619c255298caed23e0fe7ef
End2End-Transit <admin@e2etransit.com>
2025-08-05 19:45:11 +0530

    port update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(errorMiddleware);
  app.get('/', (req, res) => {
     res.send('Welcome to the subscription tracker API!');
  });
- const port = 3000;
+ const port = process.env.PORT || 3000;
  app.listen(port, async () => {
     console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
  
